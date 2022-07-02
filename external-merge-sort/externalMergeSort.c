dd1ea4cfd27b7c542ef3760cddc68ba357e82628
Thales Maia <mbthales10@gmail.com>
2022-07-02 12:08:24 -0300

    Começo do código de external merge sort

---
+++ external-merge-sort/externalMergeSort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _Endereco Endereco;
+
+struct _Endereco
+{
+	char logradouro[72];
+	char bairro[72];
+	char cidade[72];
+	char uf[72];
+	char sigla[2];
+	char cep[8];
+	char lixo[2];
+};
+
+int main(int argc, char**argv)
+{
+		int num;
+    printf("Digite a quantidade de arquivos que voce deseja gerar para a ordenacao:\n");
+    scanf("%d", &num);
+    return 0;
+}
